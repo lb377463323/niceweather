a01fdffb8ca330d749317315032aad37268e47ef
Brian <377463323@qq.com>
2015-08-04 11:02:07 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/niceweather/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,56 @@
+package com.niceweather.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Administrator on 2015/8/3.
+ */
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
+
+    /**
+     *
+     * Province表建表语句
+     */
+    public static final String CREATEE_PROVINCE = "create table Province (" +
+            "id integer primary key autoincrement, " +
+            "province_name text, " +
+            "province_code text)";
+
+    /**
+     *
+     * City表建表语句
+     */
+    public static final String CREATEE_CITY = "create table City (" +
+            "id integer primary key autoincrement, " +
+            "city_name text, " +
+            "city_code text, " +
+            "province_id interge)";
+
+    /**
+     *
+     * County表建表语句
+     */
+    public static final String CREATE_COUNTY = "create table County (" +
+            "id integer primary key autoincrement, " +
+            "county_name text, " +
+            "county_code text, " +
+            "city_id interge)";
+
+    public CoolWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATEE_PROVINCE);//创建Province表
+        db.execSQL(CREATEE_CITY);//创建City表
+        db.execSQL(CREATE_COUNTY);//创建County表
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
